f6c577bc409af1a321451ed31b984025be2010f6
John Licitra <jtl>
2019-10-09 16:23:02 -0400

    Redirect to /game after clicking the button to start is now functional and displays a game board

--- src/main/java/com/webcheckers/ui/board/BoardView.java
+++ src/main/java/com/webcheckers/ui/board/BoardView.java
@@ -1,5 +1,8 @@
 package com.webcheckers.ui.board;
 
+import com.webcheckers.model.CheckersGame;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,14 +13,45 @@ import java.util.List;
  */
 public class BoardView implements Iterable<Row> {
     /** A List containing the Rows of the game board **/
-    private List<Row> rows;
+    private ArrayList<Row> rows;
 
     /**
      * Construct a new BoardView instance
      * @param rows a List containing the Rows of the board
      */
-    public BoardView(List<Row> rows){
+    public BoardView(ArrayList<Row> rows){
         this.rows = rows;
+    }
+
+    /**
+     * Construct a new BoardView instance corresponding to a CheckersGame model
+     * @param model the model to turn into a BoardView
+     */
+    public BoardView(CheckersGame model){
+        CheckersGame.Piece[][] board = model.getBoard();
+        this.rows = new ArrayList<Row>();
+        for (int y = 0; y < board.length; y++){
+            ArrayList<Space> spaces = new ArrayList<Space>();
+            for (int x = 0; x < board.length; x++){
+                CheckersGame.Piece cgPiece = board[x][y];
+                if(cgPiece == CheckersGame.Piece.RED){
+                    spaces.add(new Space(x, y, new Piece(Piece.Type.SINGLE, Piece.Color.RED)));
+                }
+                else if(cgPiece == CheckersGame.Piece.WHITE){
+                    spaces.add(new Space(x, y, new Piece(Piece.Type.SINGLE, Piece.Color.WHITE)));
+                }
+                else if(cgPiece == CheckersGame.Piece.WHITE_KING){
+                    spaces.add(new Space(x, y, new Piece(Piece.Type.KING, Piece.Color.WHITE)));
+                }
+                else if(cgPiece == CheckersGame.Piece.RED_KING){
+                    spaces.add(new Space(x, y, new Piece(Piece.Type.KING, Piece.Color.RED)));
+                }
+                else{
+                    spaces.add(new Space(x, y, null));
+                }
+            }
+            rows.add(new Row(spaces, y));
+        }
     }
 
     /**
