3815df4210d3ad9b56a79f4c1067816a628457be
Liam Obrochta <lao1711@g.rit.edu>
2019-11-03 21:17:58 -0500

    Added body

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -1,13 +1,39 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.GameServer;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Player;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.TemplateEngine;
+
+import java.util.logging.Logger;
 
 public class PostValidateMoveRoute implements Route {
 
+
+    private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
+
+    private GameServer gameServer;
+
+    private TemplateEngine templateEngine;
+
+    public PostValidateMoveRoute(TemplateEngine templateEngine, GameServer gameServer){
+        this.gameServer = gameServer;
+        this.templateEngine = templateEngine;
+    }
+
+
     @Override
-    public Object handle(Request request, Response response) throws Exception {
+    public Object handle(Request request, Response response){
+        LOG.warning("TEST");
+        Player player = PlayerLobby.getPlayerFromSessionID(request.session().id());
+        Game game = gameServer.getGameFromPlayer(player);
+
+        System.out.println("TEST");
+
 
         return null;
     }
