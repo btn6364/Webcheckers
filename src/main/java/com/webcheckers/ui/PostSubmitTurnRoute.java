5159a0fc408a2841d5c75a3c0b6fa039036bb853
Liam Obrochta <lao1711@g.rit.edu>
2019-11-04 15:11:58 -0500

    Successfully submits a valid move to the server and updates on both ends

--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -1,13 +1,31 @@
 package com.webcheckers.ui;
 
+import com.google.gson.Gson;
+import com.webcheckers.appl.GameServer;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Move;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.TemplateEngine;
 
 public class PostSubmitTurnRoute implements Route {
 
+    private TemplateEngine templateEngine;
+
+    public PostSubmitTurnRoute(TemplateEngine engine){
+        this.templateEngine = engine;
+    }
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return null;
+        Player player = PlayerLobby.getPlayerFromSessionID(request.session().id());
+        Game game = PlayerLobby.getGameFromPlayer(player);
+        Gson gson = new Gson();
+
+        return gson.toJson(game.submitMove(player));
     }
 }
