d2c55ce5a98a54b64dba54f6689792022613d078
William O <38233971+laodev@users.noreply.github.com>
2019-11-14 16:04:20 -0500

    Create NameUtils.java
    Created and added isAlphaNum method

---
+++ src/main/java/com/webcheckers/util/NameUtils.java
@@ -0,0 +1,13 @@
+package com.webcheckers.util;
+
+public class NameUtils {
+
+    /**
+     * Check whether a username is valid or not.
+     * @param name the name passed in.
+     * @return true if the name is valid and false otherwise.
+     */
+    public static boolean isAlphaNumeric(String name){
+        return (name!=null) && (name.matches("^[a-zA-Z0-9\\s]+$") && !name.matches("\\s+"));
+    }
+}
