134623906e1246bfd18f3610e9e1a0874dc07b88
William O <38233971+laodev@users.noreply.github.com>
2019-11-14 16:04:09 -0500

    Update PlayerLobby.java
    Removed methods not associated with this class

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -102,24 +102,4 @@ public class PlayerLobby {
         removePlayer(getPlayer(identifier));
     }
 
-    /**
-     * Create a new game for 2 players.
-     * @param player1 player 1.
-     * @param player2 player 2.
-     */
-    public void newGame(Player player1, Player player2){
-        Game g = new Game(player1, player2);
-        server.addGame(g);
-    }
-
-
-
-    /**
-     * Check whether a username is valid or not.
-     * @param name the name passed in.
-     * @return true if the name is valid and false otherwise.
-     */
-    public boolean isAlphaNumeric(String name){
-        return (name!=null) && (name.matches("^[a-zA-Z0-9\\s]+$") && !name.matches("\\s+"));
-    }
 }
