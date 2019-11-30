fd182d82bb694136e5b5295c25b964f21274fd40
Liam Obrochta <lao1711@g.rit.edu>
2019-11-29 20:01:36 -0500

    Modified resignGame function

--- src/main/java/com/webcheckers/appl/GameServer.java
+++ src/main/java/com/webcheckers/appl/GameServer.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 /**
  * A server backend which maintains a list of logged-in players and in-progress games.
+ *
  * @author Bao Nguyen
  * @author Liam Obrochta
  * @author John Licitra
@@ -16,16 +17,18 @@ public class GameServer {
 
     /**
      * Create a new game for 2 players.
+     *
      * @param player1 player 1.
      * @param player2 player 2.
      */
-    public void newGame(Player player1, Player player2){
+    public void newGame(Player player1, Player player2) {
         Game g = new Game(player1, player2);
         addGame(g);
     }
 
     /**
      * Add a new Game to the list of games being played
+     *
      * @param game the game to add
      */
     public void addGame(Game game) {
@@ -34,20 +37,22 @@ public class GameServer {
 
     /**
      * Remove a game from the gameInProgress collection.
+     *
      * @param game the game to remove.
      */
-    public void removeGame(Game game) {
-        this.gamesInProgress.remove(game);
+    public boolean removeGame(Game game) {
+        return this.gamesInProgress.remove(game);
     }
 
     /**
      * Get the Game that a Player is playing
+     *
      * @param player the player whose game will be returned
      * @return null if the player is not in a game, otherwise, their game
      */
     public Game getGame(Player player) {
-        for (Game game : gamesInProgress){
-            if (game.contains(player)){
+        for (Game game : gamesInProgress) {
+            if (game.contains(player)) {
                 return game;
             }
         }
@@ -56,6 +61,7 @@ public class GameServer {
 
     /**
      * Get the ArrayList of games currently being played
+     *
      * @return gamesInProgress
      */
     public ArrayList getGamesInProgress() {
@@ -64,14 +70,14 @@ public class GameServer {
 
     /**
      * Resign a player from the game.
+     *
      * @param player the player in the game.
      */
-    public void resignGame(Player player){
+    public boolean resignGame(Player player) {
         Game g = getGame(player);
-        g.setResign(true);
-        removeGame(g);
-    }
+        return g.setResign(player);
 
+    }
 
 
 }
