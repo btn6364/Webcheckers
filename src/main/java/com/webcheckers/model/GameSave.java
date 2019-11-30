589c7fa952a928075bb87029ddbe1284d3399f51
Liam Obrochta <lao1711@g.rit.edu>
2019-11-30 00:39:19 -0500

    Holds game save state data for later replay

---
+++ src/main/java/com/webcheckers/model/GameSave.java
@@ -0,0 +1,28 @@
+package com.webcheckers.model;
+
+import com.webcheckers.ui.board.BoardView;
+
+import java.util.ArrayList;
+
+public class GameSave {
+
+    private Player player;
+    private Game game;
+    private ArrayList<BoardView> viewSaves;
+
+    public GameSave(Player player, Game game, ArrayList<BoardView> saves){
+        this.player = player;
+        this.game = game;
+        this.viewSaves = saves;
+    }
+
+
+    public String getName(){
+        return this.game.getName();
+    }
+
+    public String getGameID(){
+        return this.game.getGameID();
+    }
+
+}
