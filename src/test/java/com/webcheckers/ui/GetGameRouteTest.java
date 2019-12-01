6d250d6b415ca44678310a4f8fd9a960ffc9affb
John Licitra <jtl>
2019-12-01 03:35:21 -0500

    Fixed broken test classes

--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -1,27 +1,22 @@
 package com.webcheckers.ui;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.webcheckers.appl.GameServer;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
-import com.webcheckers.ui.board.BoardView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import spark.HaltException;
+import com.google.gson.Gson;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
-import spark.http.matching.Halt;
 
 /**
  * The unit test suite for the {@link GetGameRoute} component.
@@ -64,7 +59,7 @@ public class GetGameRouteTest {
         gameServer = mock(GameServer.class);
 
         //Create a unique CuT for each test.
-        CuT = new GetGameRoute(engine, playerLobby, gameServer);
+        CuT = new GetGameRoute(engine, playerLobby, gameServer, new Gson());
     }
 
     @Test
