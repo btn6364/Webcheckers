2b8abfd43d31b3254de38cb54db9624c76fe7b2b
Liam Obrochta <lao1711@g.rit.edu>
2019-11-30 19:14:07 -0500

    Syntax fixes

--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -1,27 +1,24 @@
 package com.webcheckers.ui;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.gson.Gson;
 import com.webcheckers.appl.GameServer;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
-import com.webcheckers.ui.board.BoardView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import spark.HaltException;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
-import spark.http.matching.Halt;
 
 /**
  * The unit test suite for the {@link GetGameRoute} component.
@@ -48,6 +45,7 @@ public class GetGameRouteTest {
 
     private PlayerLobby playerLobby;
     private GameServer gameServer;
+    private Gson gson;
 
 
     @BeforeEach
@@ -62,9 +60,10 @@ public class GetGameRouteTest {
         engine = mock(TemplateEngine.class);
         playerLobby = mock(PlayerLobby.class);
         gameServer = mock(GameServer.class);
+        gson = mock(Gson.class);
 
         //Create a unique CuT for each test.
-        CuT = new GetGameRoute(engine, playerLobby, gameServer);
+        CuT = new GetGameRoute(engine, playerLobby, gameServer, gson);
     }
 
     @Test
